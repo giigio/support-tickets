deabb911d41cd85136cc105161a722134035b015
Giovanna Leme Amadeu <80579735+giigio@users.noreply.github.com>
2025-06-23 16:50:44 -0300

    implement ticket creation with detailed response

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto";
+
 export function create({ res, req }) {
-  return res.end("Ticket created successfully");
+  const { equipment, description, user_name } = req.body;
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
+  return res.end(JSON.stringify(ticket));
 }
