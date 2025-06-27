9ab447af0000ee3d7d0598683eb663e5b3ce0598
Giovanna Leme Amadeu <80579735+giigio@users.noreply.github.com>
2025-06-27 09:38:46 -0300

    add ticket update logic on controller

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,12 @@
 export function update({ req, res, database }) {
-  return res.end("ok");
+  const { id } = req.params;
+  const { equipment, description } = req.body;
+
+  database.update("tickets", id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+  });
+
+  return res.end();
 }
